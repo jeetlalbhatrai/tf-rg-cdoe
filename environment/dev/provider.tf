d1a542c2f91c43b4bfc0bd59b979936c9dbe835f
jeetbhatrai <58354081+jeetlalbhatrai@users.noreply.github.com>
2025-08-31 22:31:04 +0530

    Update provider.tf

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-  # backend "azurerm" {
+ backend "azurerm" {
     subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
     resource_group_name  = "jeet-rg"
     storage_account_name = "tfstatefilestodoapp"
